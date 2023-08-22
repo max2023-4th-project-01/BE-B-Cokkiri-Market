6d98d4469bf51fb5884eb7e6712932be7bab4586
Jay <chlwodnjs34@gmail.com>
2023-08-22 17:22:52 +0900

    style #1 : prettier 설정 변경으로 인한 수정

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,14 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { ThemeProvider } from "styled-components";
-import { App } from "./App.tsx";
-import { designSystem } from "./styles/designSystem.ts";
-import "./styles/index.css";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { ThemeProvider } from 'styled-components';
+import { App } from './App.tsx';
+import { designSystem } from './styles/designSystem.ts';
+import './styles/index.css';
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ThemeProvider theme={designSystem}>
       <App />
     </ThemeProvider>
-  </React.StrictMode>,
+  </React.StrictMode>
 );
