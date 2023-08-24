b36ce834156a1266fa677b4df6be61499463911e
yhpark95 <98851575+yhpark95@users.noreply.github.com>
2023-08-24 12:53:23 +0900

    init: 프로젝트 초기 환경 세팅

---
+++ second-hand/src/test/java/kr/codesquad/SecondHandApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.codesquad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecondHandApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
