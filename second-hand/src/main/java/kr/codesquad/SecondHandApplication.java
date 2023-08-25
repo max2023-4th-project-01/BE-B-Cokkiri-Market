9a0973ed8d48b3034bcb64b4a0b096c51f5cb058
HyowonSin <tlsgydnjs@naver.com>
2023-08-25 12:10:52 +0900

    fix: zoned date time 설정

--- second-hand/src/main/java/kr/codesquad/SecondHandApplication.java
+++ second-hand/src/main/java/kr/codesquad/SecondHandApplication.java
@@ -1,13 +1,26 @@
 package kr.codesquad;
 
+import kr.codesquad.util.TimeStamped;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.auditing.DateTimeProvider;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@SpringBootApplication
+import java.time.ZonedDateTime;
+import java.util.Optional;
+
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+@EnableJpaAuditing(dateTimeProviderRef = "zonedDateTimeProvider")
 public class SecondHandApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SecondHandApplication.class, args);
 	}
 
+	@Bean
+	public DateTimeProvider zonedDateTimeProvider(){
+		return () -> Optional.of(ZonedDateTime.now(TimeStamped.SEOUL_ZONE_ID));
+	}
 }
