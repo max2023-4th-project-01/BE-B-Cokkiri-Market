cbf20b2c7af82b43774f55c2d585391f5b652807
fuse <massenger7@gmail.com>
2023-08-27 23:44:46 +0900

    chore #15: Zustand 테스트 및 devtools 테스트

--- src/store.ts
+++ src/store.ts
@@ -1,4 +1,5 @@
 import { create } from 'zustand';
+import { devtools } from 'zustand/middleware';
 
 type State = {
   count: number;
@@ -9,3 +10,22 @@ export const countStore = create<State>(set => ({
   count: 0,
   increment: () => set(state => ({ count: state.count + 1 })),
 }));
+
+type NameState = {
+  firstName: string;
+  lastName: string;
+};
+
+type NameAction = {
+  updateFirstName: (firstName: NameState['firstName']) => void;
+  updateLastName: (lastName: NameState['lastName']) => void;
+};
+
+export const useNameStore = create<NameState & NameAction>()(
+  devtools(set => ({
+    firstName: '',
+    lastName: '',
+    updateFirstName: firstName => set({ firstName }),
+    updateLastName: lastName => set({ lastName }),
+  }))
+);
