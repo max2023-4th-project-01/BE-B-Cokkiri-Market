3ad8bb40089ee6c7950367e47ba3b133ade3cd91
yhpark95 <98851575+yhpark95@users.noreply.github.com>
2023-08-28 23:42:24 +0900

    refactor: 불필요한 요소 제거 #1

--- second-hand/src/main/java/kr/codesquad/auth/OAuthService.java
+++ second-hand/src/main/java/kr/codesquad/auth/OAuthService.java
@@ -8,20 +8,25 @@ import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserServ
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
 
-import kr.codesquad.user.Role;
+import kr.codesquad.location.Location;
+import kr.codesquad.location.LocationRepository;
 import kr.codesquad.user.User;
 import kr.codesquad.user.UserRepository;
+import lombok.RequiredArgsConstructor;
 
 @Service
+@RequiredArgsConstructor
 public class OAuthService implements OAuth2UserService<OAuth2UserRequest, OAuth2User> {
+	private final static String GITHUB_ID = "id";
+	private final static String GITHUB_PROFILE_URL = "avatar_url";
+	private final static String GITHUB_LOGIN_ID = "login";
+	private final static String GITHUB_SUFFIX = "_github";
 	private final UserRepository userRepository;
-
-	public OAuthService(UserRepository userRepository) {
-		this.userRepository = userRepository;
-	}
+	private final LocationRepository locationRepository;
 
 	@Override
 	public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
@@ -29,19 +34,23 @@ public class OAuthService implements OAuth2UserService<OAuth2UserRequest, OAuth2
 		OAuth2User oAuth2User = delegate.loadUser(userRequest); // OAuth 서비스(github, google, naver)에서 가져온 유저 정보를 담고있음
 
 		Map<String, Object> attributes = oAuth2User.getAttributes(); // OAuth 서비스의 유저 정보들
+		Integer id = (Integer)attributes.get(GITHUB_ID);
 		User user = User.builder()
-			.loginId((String)attributes.get("login"))
-			.profileImage((String)attributes.get("avatar_url"))
-			.role(Role.GUEST)
+			.loginId(id.toString())
+			.profileImage((String)attributes.get(GITHUB_PROFILE_URL))
+			.nickName(attributes.get(GITHUB_LOGIN_ID) + GITHUB_SUFFIX)
 			.build();
 
 		if (!userRepository.existsByLoginId(user.getLoginId())) {
-			userRepository.save(user);
+			User savedUser = userRepository.save(user);
+			// TODO: 기본 동네 반환 메소드 구현 필요
+			locationRepository.save(Location.builder()
+				.userId(savedUser.getId())
+				.locationName("기본동네")
+				.build());
 		}
 
-		return new CustomOAuth2User(
-			Collections.singleton(new SimpleGrantedAuthority(user.getRole().getValue())),
-			attributes,
-			"login", userRepository.findByLoginId(user.getLoginId()));
+		return new DefaultOAuth2User(Collections.singleton(new SimpleGrantedAuthority("no_role"))
+			, attributes, GITHUB_ID);
 	}
 }
--- second-hand/src/main/java/kr/codesquad/jwt/JwtAuthenticationSuccessHandler.java
+++ second-hand/src/main/java/kr/codesquad/jwt/JwtAuthenticationSuccessHandler.java
@@ -1,7 +1,6 @@
 package kr.codesquad.jwt;
 
 import java.io.IOException;
-import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -9,46 +8,29 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.http.MediaType;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-import kr.codesquad.user.Role;
-import kr.codesquad.user.User;
-import kr.codesquad.user.UserRepository;
+import lombok.RequiredArgsConstructor;
 
 @Component
+@RequiredArgsConstructor
 public class JwtAuthenticationSuccessHandler implements AuthenticationSuccessHandler {
-
 	private final JwtProvider jwtProvider;
-	private final UserRepository userRepository;
-
-	public JwtAuthenticationSuccessHandler(JwtProvider jwtProvider, UserRepository userRepository) {
-		this.jwtProvider = jwtProvider;
-		this.userRepository = userRepository;
-	}
 
 	@Override
 	public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
 		Authentication authentication) throws IOException, ServletException {
 
-		User user = userRepository.findByLoginId(authentication.getName());
-		if (user.getRole() == Role.GUEST) {
-			String token = jwtProvider.createToken(Map.of("id", user.getId()),
-				new Date(System.currentTimeMillis() + 300000));
-			response.addHeader("Authorization", "Bearer " + token);
-			response.sendRedirect("/sign-up");
-			return;
-
-		}
-
-		Jwt jwt = jwtProvider.createJwt(Map.of("id", user.getId()));
+		Jwt jwt = jwtProvider.createJwt(Map.of("id", authentication.getName()));
 		Map<String, Object> responseMap = new HashMap<>();
 		responseMap.put("jwt", jwt);
 
-		response.setContentType("application/json");
+		response.setContentType(MediaType.APPLICATION_JSON_VALUE);
 		response.getWriter().write(new ObjectMapper().writeValueAsString(responseMap));
 	}
 }
