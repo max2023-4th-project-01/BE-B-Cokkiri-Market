eece332fa6866d36c8f6621a9c0170d84fddd2a7
yhpark95 <98851575+yhpark95@users.noreply.github.com>
2023-08-28 23:43:22 +0900

    feat: securityContext에 인증 되었다는 정보 추가 #1

--- second-hand/src/main/java/kr/codesquad/common/AuthorizationFilter.java
+++ second-hand/src/main/java/kr/codesquad/common/AuthorizationFilter.java
@@ -1,6 +1,7 @@
 package kr.codesquad.common;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
@@ -9,6 +10,8 @@ import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.util.PatternMatchUtils;
 import org.springframework.web.cors.CorsUtils;
 
@@ -17,22 +20,19 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.MalformedJwtException;
 import kr.codesquad.jwt.JwtProvider;
+import lombok.RequiredArgsConstructor;
 
+@RequiredArgsConstructor
 public class AuthorizationFilter implements Filter {
 	private static final String TOKEN_PREFIX = "Bearer ";
 	private static final String HEADER_AUTHORIZATION = "Authorization";
 	private static final String USER_ID = "id";
-	private static final String[] whiteListUris = {"/h2-console/**", "/sign-up", "/api/members/signup",
+	private static final String[] whiteListUris = {"/h2-console/**", "/api/signup", "/api/login",
 		"/api/members/signup/**",
-		"/api/reissue-access-token", "/api/oauth/**", "/api/redirect/**", "/test"};
+		"/api/reissue-access-token", "/api/oauth/**", "/api/redirect/**"};
 
 	private final JwtProvider jwtProvider;
 	private final ObjectMapper objectMapper;
-
-	public AuthorizationFilter(ObjectMapper objectMapper, JwtProvider jwtProvider) {
-		this.jwtProvider = jwtProvider;
-		this.objectMapper = objectMapper;
-	}
 
 	@Override
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
@@ -56,6 +56,9 @@ public class AuthorizationFilter implements Filter {
 			String token = getToken(httpServletRequest);
 			Claims claims = jwtProvider.getClaims(token);
 			request.setAttribute(USER_ID, claims.get(USER_ID));
+			SecurityContextHolder.getContext()
+				.setAuthentication(
+					new UsernamePasswordAuthenticationToken(claims.getSubject(), null, new ArrayList<>()));
 			chain.doFilter(request, response);
 		} catch (RuntimeException e) {
 			throw new MalformedJwtException("");
