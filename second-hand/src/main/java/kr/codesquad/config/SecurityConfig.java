cbea2be32a721525cfadcf305b36fe619f5e9fca
yhpark95 <98851575+yhpark95@users.noreply.github.com>
2023-08-28 23:41:13 +0900

    feat: 일반 로그인 필터 구현 및 설정 #1

--- second-hand/src/main/java/kr/codesquad/config/SecurityConfig.java
+++ second-hand/src/main/java/kr/codesquad/config/SecurityConfig.java
@@ -1,31 +1,80 @@
 package kr.codesquad.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.authentication.logout.LogoutFilter;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import kr.codesquad.auth.OAuthService;
+import kr.codesquad.common.AuthenticationFilter;
+import kr.codesquad.common.AuthorizationFilter;
 import kr.codesquad.jwt.JwtAuthenticationSuccessHandler;
+import kr.codesquad.jwt.JwtProvider;
+import kr.codesquad.user.UserService;
+import lombok.RequiredArgsConstructor;
 
 @EnableWebSecurity // spring security 설정을 활성화시켜주는 어노테이션
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
-
+@Configuration
+@RequiredArgsConstructor
+public class SecurityConfig {
+	private final UserService userService;
+	private final BCryptPasswordEncoder bCryptPasswordEncoder;
 	private final OAuthService oAuthService;
 	private final JwtAuthenticationSuccessHandler jwtAuthenticationSuccessHandler;
+	private final JwtProvider jwtProvider;
 
-	public SecurityConfig(OAuthService oAuthService, JwtAuthenticationSuccessHandler jwtAuthenticationSuccessHandler) {
-		this.oAuthService = oAuthService;
-		this.jwtAuthenticationSuccessHandler = jwtAuthenticationSuccessHandler;
-	}
+	@Bean
+	protected SecurityFilterChain configure(HttpSecurity http) throws Exception {
+		AuthenticationManagerBuilder authenticationManagerBuilder = http.getSharedObject(
+			AuthenticationManagerBuilder.class);
 
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		http.csrf().disable() // h2 console 접속을 위해
-			.headers().frameOptions().disable() // h2 console 접속을 위해
+		authenticationManagerBuilder.userDetailsService(userService).passwordEncoder(bCryptPasswordEncoder);
+		AuthenticationManager authenticationManager = authenticationManagerBuilder.build();
+
+		AuthenticationFilter authenticationFilter = new AuthenticationFilter(authenticationManager);
+		authenticationFilter.setAuthenticationSuccessHandler(jwtAuthenticationSuccessHandler);
+		authenticationFilter.setFilterProcessesUrl("/api/login");
+
+		AuthorizationFilter authorizationFilter = new AuthorizationFilter(jwtProvider, new ObjectMapper());
+
+		http.csrf()
+			.disable()
+			.headers()
+			.frameOptions()
+			.disable()
+			.and()
+			.sessionManagement()
+			.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+			.and()
+			.authorizeRequests()
+			.antMatchers("/h2-console/**", "/api/signup")
+			.permitAll()
+			.anyRequest()
+			.authenticated()  //다른 요청은 인증 필요함
 			.and()
 			.oauth2Login()
 			.successHandler(jwtAuthenticationSuccessHandler)// OAuth2 로그인 설정 시작점
 			.userInfoEndpoint() // OAuth2 로그인 성공 이후 사용자 정보를 가져올 때 설정 담당
-			.userService(oAuthService); // OAuth2 로그인 성공 시, 후작업을 진행할 UserService 인터페이스 구현체 등록
+			.userService(oAuthService); // OAuth2 로그인 성공 시, 후작업을 진행할 UserService 인터페이스 구현체 등
+
+		http.formLogin()
+			.disable()
+			.httpBasic()
+			.disable()
+			.authenticationManager(authenticationManager)
+			.addFilterAfter(authenticationFilter, LogoutFilter.class)
+			.addFilterBefore(authorizationFilter, UsernamePasswordAuthenticationFilter.class);
+
+		return http.build();
 	}
+
 }
