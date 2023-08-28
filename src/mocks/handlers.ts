ef0bbe1e73e05ef95974e99ac2b3fa334485f798
jay <chlwodnjs34@gmail.com>
2023-08-28 23:53:19 +0900

    feat #29 : msw mainHandler 추가

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,9 +1,11 @@
 import { rest } from 'msw';
 import { authHandlers } from './authHandlers';
+import { mainHandlers } from './mainHandlers';
 
 export const handlers = [
   rest.get('/api/test', (_, res, ctx) => {
     return res(ctx.status(200), ctx.json('hello'));
   }),
   ...authHandlers,
+  ...mainHandlers,
 ];
