a831fa189cc8bc3d4ca7a3c8f4cac7e9b0a76863
sudago <sudago22@gmail.com>
2023-09-01 11:29:32 +0900

    fix: cors 에러 설정 #7

--- be/src/main/java/kr/codesquad/config/SecurityConfig.java
+++ be/src/main/java/kr/codesquad/config/SecurityConfig.java
@@ -12,6 +12,9 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.authentication.logout.LogoutFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import kr.codesquad.auth.OAuthService;
 import kr.codesquad.filter.AuthenticationFilter;
@@ -51,6 +54,8 @@ public class SecurityConfig {
 			.frameOptions()
 			.disable()
 			.and()
+			.cors().configurationSource(configurationSource())
+			.and()
 			.sessionManagement()
 			.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
 			.and()
@@ -75,6 +80,18 @@ public class SecurityConfig {
 			.addFilterBefore(authorizationFilter, UsernamePasswordAuthenticationFilter.class);
 
 		return http.build();
+	}
+
+	public CorsConfigurationSource configurationSource() {
+		CorsConfiguration configuration = new CorsConfiguration();
+		configuration.addAllowedHeader("*");
+		configuration.addAllowedMethod("*"); // GET, POST, PUT, DELETE (Javascript 요청 허용)
+		configuration.addAllowedOriginPattern("*"); // 모든 IP 주소 허용 (프론트 앤드 IP만 허용 react)
+		configuration.setAllowCredentials(true); // 클라이언트에서 쿠키 요청 허용
+		configuration.addExposedHeader("Authorization"); // 옛날에는 디폴트 였다. 지금은 아닙니다.
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", configuration);
+		return source;
 	}
 
 }
