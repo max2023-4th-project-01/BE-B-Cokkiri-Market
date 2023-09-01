ffb967766ca421d679ad8097efd107217c8b5308
HyowonSin <tlsgydnjs@naver.com>
2023-09-01 17:30:53 +0900

    fix: 동네 검색 반환값 명세 맞춤

--- be/src/main/java/kr/codesquad/location/controller/LocationController.java
+++ be/src/main/java/kr/codesquad/location/controller/LocationController.java
@@ -1,6 +1,7 @@
 package kr.codesquad.location.controller;
 
 import java.util.List;
+import java.util.Map;
 
 import kr.codesquad.location.dto.response.LocationListResponse;
 import kr.codesquad.location.service.LocationService;
@@ -13,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class LocationController {
 
     @GetMapping("/api/locations")
-    public ResponseEntity<List<LocationListResponse>> getLocations(@RequestParam String query) {
-        return ResponseEntity.ok(LocationService.getLocations(query));
+    public ResponseEntity<Map<String, List<LocationListResponse>>> getLocations(@RequestParam String query) {
+        return ResponseEntity.ok(Map.of("locations", LocationService.getLocations(query)));
     }
 }
