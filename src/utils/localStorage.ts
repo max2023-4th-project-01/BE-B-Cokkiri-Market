ebdf4084048bf47fd9a1c9e740922d3cdbedd7d4
Jay <chlwodnjs34@gmail.com>
2023-09-05 18:39:50 +0900

    refactor #59 : persist를 활용한 localStorage 저장
    - 불필요한 localStorage 함수 제거 및 key에 맞는 get 함수 구현

--- src/utils/localStorage.ts
+++ src/utils/localStorage.ts
@@ -1,28 +1,49 @@
-export const setAccessToken = (accessToken: string) => {
-  localStorage.setItem('accessToken', accessToken);
-};
-
-export const setUserInfo = (userInfo: {
-  username: string;
-  profileImageUrl: string;
-}) => {
-  localStorage.setItem('userInfo', JSON.stringify(userInfo));
+type AuthStorage = {
+  state: {
+    accessToken: string;
+    refreshToken: string;
+    nickname: string;
+    profileImageUrl: string;
+    isLogin: boolean;
+    username: string;
+  };
+  version: number;
 };
 
 export const getAccessToken = () => {
-  return localStorage.getItem('accessToken');
+  const authStorageJSON = localStorage.getItem('auth-storage');
+  const authStorage: AuthStorage = authStorageJSON
+    ? JSON.parse(authStorageJSON)
+    : null;
+
+  return authStorage?.state.accessToken || null;
 };
 
-export const getUserInfo = (): {
-  username: string;
-  profileImageUrl: string;
-} | null => {
-  const userInfoJSON = localStorage.getItem('userInfo');
+export const getRefreshToken = () => {
+  const authStorageJSON = localStorage.getItem('auth-storage');
+  const authStorage: AuthStorage = authStorageJSON
+    ? JSON.parse(authStorageJSON)
+    : null;
 
-  return userInfoJSON ? JSON.parse(userInfoJSON) : null;
+  return authStorage?.state.refreshToken || null;
+};
+
+export const getUserInfo = () => {
+  const authStorageJSON = localStorage.getItem('auth-storage');
+  const authStorage: AuthStorage = authStorageJSON
+    ? JSON.parse(authStorageJSON)
+    : null;
+
+  if (authStorage) {
+    return {
+      nickname: authStorage.state.nickname,
+      profileImageUrl: authStorage.state.profileImageUrl,
+    };
+  }
+
+  return null;
 };
 
 export const clearAuthInfo = () => {
-  localStorage.removeItem('accessToken');
-  localStorage.removeItem('userInfo');
+  localStorage.removeItem('auth-storage');
 };
