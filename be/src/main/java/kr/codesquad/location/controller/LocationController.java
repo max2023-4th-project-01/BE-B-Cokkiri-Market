6c79bc5facd024a59b4745f4ff5978ccd2996624
HyowonSin <tlsgydnjs@naver.com>
2023-09-05 17:43:43 +0900

    chore: 로케이션 컨트롤러 스태틱 누락, 카테고리 띄어쓰기

--- be/src/main/java/kr/codesquad/location/controller/LocationController.java
+++ be/src/main/java/kr/codesquad/location/controller/LocationController.java
@@ -5,16 +5,20 @@ import java.util.Map;
 
 import kr.codesquad.location.dto.response.LocationListResponse;
 import kr.codesquad.location.service.LocationService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 public class LocationController {
+
+    private final LocationService locationService;
 
     @GetMapping("/api/locations")
     public ResponseEntity<Map<String, List<LocationListResponse>>> getLocations(@RequestParam String query) {
-        return ResponseEntity.ok(Map.of("locations", LocationService.getLocations(query)));
+        return ResponseEntity.ok(Map.of("locations", locationService.getLocations(query)));
     }
 }
