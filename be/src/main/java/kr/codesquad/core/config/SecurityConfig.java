0fd89872e9bce148f9a44c2c0e40320f39455540
yhpark95 <98851575+yhpark95@users.noreply.github.com>
2023-09-08 00:51:53 +0900

    fix: Oauth 로그인 url security에서 열어줌 #1

--- be/src/main/java/kr/codesquad/core/config/SecurityConfig.java
+++ be/src/main/java/kr/codesquad/core/config/SecurityConfig.java
@@ -63,7 +63,7 @@ public class SecurityConfig {
 			.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
 			.and()
 			.authorizeRequests()
-			.antMatchers("/h2-console/**", "/api/locations**").permitAll()
+			.antMatchers("/h2-console/**", "/api/locations**", "/login/**").permitAll()
 			.antMatchers(HttpMethod.POST, "/api/users").permitAll()
 			.anyRequest()
 			.authenticated()  //다른 요청은 인증 필요함
