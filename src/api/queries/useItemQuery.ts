061e343613aa434e331bec576d6b157346467e33
fuse <massenger7@gmail.com>
2023-09-11 17:12:53 +0900

    refactor #78: 판매내역의 useQuery -> useInfiniteQuery 커스텀훅으로 분리

--- src/api/queries/useItemQuery.ts
+++ src/api/queries/useItemQuery.ts
@@ -1,14 +1,16 @@
 import { useInfiniteQuery, useQuery } from '@tanstack/react-query';
+import { SalesListData } from '../../page/SalesList';
 import { ItemData, categoryDataType } from '../../types';
 import {
   getFavoritesCategories,
   getFavoritesItemData,
   getItems,
-} from '../mainFetcher';
+  getSalesList,
+} from '../itemFetcher';
 
 const ITEMS_QUERY_KEY = 'items';
 const SALES_LIST_QUERY_KEY = 'salesList';
-const FAVORITES_QUERY_KEY = 'favorites';
+// const FAVORITES_QUERY_KEY = 'favorites';
 
 export const useGetItemData = (categoryId: number | null) => {
   return useInfiniteQuery<ItemData>(
@@ -20,15 +22,31 @@ export const useGetItemData = (categoryId: number | null) => {
   );
 };
 
+export const useGetSalesList = ({
+  nickname,
+  isSold,
+}: {
+  nickname: string;
+  isSold?: boolean;
+}) => {
+  return useInfiniteQuery<SalesListData>(
+    [SALES_LIST_QUERY_KEY, nickname, isSold],
+    ({ pageParam }) => getSalesList({ pageParam, nickname, isSold }),
+    {
+      getNextPageParam: lastPage => lastPage.nextCursor ?? undefined,
+    }
+  );
+};
+
 export const useGetFavoritesCategoryData = () => {
-  return useQuery<categoryDataType, Error>(
+  return useQuery<categoryDataType>(
     ['favoritesCategory'],
     getFavoritesCategories
   );
 };
 
 export const useGetFavoritesItemData = (categoryId?: number) => {
-  return useQuery<ItemData, Error>(['favoritesItems', categoryId], () =>
+  return useQuery<ItemData>(['favoritesItems', categoryId], () =>
     getFavoritesItemData(categoryId)
   );
 };
