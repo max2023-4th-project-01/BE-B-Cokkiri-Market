d857f4cd0661173941b7f4feac9a40d109a43065
sudago <sudago22@gmail.com>
2023-09-12 18:35:37 +0900

    refactor: test 환경 설정 yml 파일로 분리 #59

--- be/src/test/java/kr/codesquad/IntegrationTestSupport.java
+++ be/src/test/java/kr/codesquad/IntegrationTestSupport.java
@@ -3,7 +3,7 @@ package kr.codesquad;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
-@ActiveProfiles("test")
+@ActiveProfiles("dev")
 @SpringBootTest
 public abstract class IntegrationTestSupport {
 
