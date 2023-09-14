8fb502f719cb454a12ab6594e99fb548845d40f8
HyowonSin <tlsgydnjs@naver.com>
2023-09-14 14:25:08 +0900

    test: 테스트 정리

--- be/src/test/java/kr/codesquad/IntegrationTestSupport.java
+++ be/src/test/java/kr/codesquad/IntegrationTestSupport.java
@@ -3,8 +3,12 @@ package kr.codesquad;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
-@ActiveProfiles("dev")
+@ActiveProfiles("test")
 @SpringBootTest
 public abstract class IntegrationTestSupport {
+
+    static {
+        System.setProperty("com.amazonaws.sdk.disableEc2Metadata", "true");
+    }
 
 }
