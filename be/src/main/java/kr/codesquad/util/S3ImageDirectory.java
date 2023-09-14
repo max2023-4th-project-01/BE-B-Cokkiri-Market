249b98f0b27af49b4c0952f1d2644df3825368c8
sudago <sudago22@gmail.com>
2023-09-14 16:49:26 +0900

    feat: S3 파일 삭제 기능 구현 #64

---
+++ be/src/main/java/kr/codesquad/util/S3ImageDirectory.java
@@ -0,0 +1,23 @@
+package kr.codesquad.util;
+
+import java.util.Arrays;
+
+import kr.codesquad.core.error.CustomException;
+import kr.codesquad.core.error.statuscode.FileErrorCode;
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public enum S3ImageDirectory {
+	// root directory 에는 파일 저장 불가. 하위 directory 안에만 파일 저장 가능
+
+	PROFILE_IMAGE("profileImage"),
+	ITEM_IMAGE("itemImage");
+
+	private final String name;
+
+	public static String findDirectory(String fileUrl) {
+		return Arrays.stream(S3ImageDirectory.values())
+			.filter(dir -> fileUrl.contains(dir.name()))
+			.findFirst().orElseThrow(() -> new CustomException(FileErrorCode.DIRECTROTY_NOT_FOUND)).name();
+	}
+}
