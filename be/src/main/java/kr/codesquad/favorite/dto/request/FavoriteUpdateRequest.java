ca63e2853cbccfb1c325605dd8718b695bb1a52f
sudago <sudago22@gmail.com>
2023-09-15 11:05:42 +0900

    fix: requestDto 에 기본 생성자 추가

--- be/src/main/java/kr/codesquad/favorite/dto/request/FavoriteUpdateRequest.java
+++ be/src/main/java/kr/codesquad/favorite/dto/request/FavoriteUpdateRequest.java
@@ -2,9 +2,11 @@ package kr.codesquad.favorite.dto.request;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 public class FavoriteUpdateRequest {
 	private Boolean isFavorite;
 
